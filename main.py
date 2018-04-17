c2b9ca9ebaf8167a5d6cb81da111f8d0b9a72acb
jcsizudev <takeshi.izu@gmail.com>
2018-04-17 16:00:26 +0900

    試し修正

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  name = '誰か'
+  name = '誰なんだか'
   return render_template('hello.html', title='Flask test', name=name)
 
 if __name__ == '__main__':
